9baec4ae5a9ec8108c180dad63a26f56741f2fdd
Drewbie <drew@drewtempleton.com>
2012-11-26 18:42:24 -0600

    Added Timepicker & Happenings

--- js/timepicker.js
+++ js/timepicker.js
@@ -1,54 +1,3 @@
-$(document).ready(function() {
-	
-	//opentable
-	$(function() {  
-        $("#datepicker").datepicker({  
-            onSelect: function(date) {
-				var a_date = date.split('/');
-				document.getElementById('ResMonth').value = a_date[0];
-				document.getElementById('ResDate').value = a_date[1];
-				document.getElementById('ResYear').value = a_date[2];
-    		},
-			dateFormat: 'm/d/yy'
-        });
-    });	
-	
-	$("#dl_PartySize dt").click(function() {
-		$("#dl_PartySize dd ul").toggle();
-	});
-	$("#dl_PartySize dd ul li").click(function() {
-		var text = $(this).html();
-		document.forms['reservations'].elements['PartySize'].value = text;
-		$("#dl_PartySize dd ul").hide();
-	});	
-	
-	//Opentable Drop down	
-	$("#dl_ResHour dt").click(function() {
-		$("#dl_ResHour dd ul").toggle();
-	});
-	$("#dl_ResHour dd ul li").click(function() {
-		var text = $(this).html();
-		document.forms['reservations'].elements['ResTime'].value = text;
-		$("#dl_ResHour dd ul").hide();
-		var a_time = text.split(':');
-		var resHour = a_time[0];
-		var b_time = a_time[1];
-		var c_time = b_time.split(' ');
-		var resMin = c_time[0];
-		resMeridian = c_time[1];
-		document.forms['reservations'].elements['ResHour'].value = resHour;
-		document.forms['reservations'].elements['ResMinute'].value = resMin;
-		document.forms['reservations'].elements['Meridian'].value = resMeridian;
-	});
-	
-	//Drop Down Global Mouseleave:	
-	$("dd ul").mouseleave(function() {
-        $("dd ul").hide();
-    });
-
-//end document ready
-});
-
 /*
  * jQuery timepicker addon
  * By: Trent Richardson [http://trentrichardson.com]
@@ -95,7 +44,7 @@ $(document).ready(function() {
 			closeText: 'Done',
 			amNames: ['AM', 'A'],
 			pmNames: ['PM', 'P'],
-			timeFormat: 'hh:mm tt',
+			timeFormat: 'HH:mm',
 			timeSuffix: '',
 			timeOnlyTitle: 'Choose Time',
 			timeText: 'Time',
@@ -116,7 +65,7 @@ $(document).ready(function() {
 			showTimezone: false,
 			showTime: true,
 			stepHour: 1,
-			stepMinute: 15,
+			stepMinute: 1,
 			stepSecond: 1,
 			stepMillisec: 1,
 			hour: 0,
@@ -1931,10 +1880,3 @@ $(document).ready(function() {
 	$.timepicker.version = "1.1.1";
 
 })(jQuery);
-
-$(document).ready(function() {
-                   $(function() {
-            $('#timepicker').datetimepicker();
-               });
-            });
-
